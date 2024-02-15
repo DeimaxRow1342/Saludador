cb54bb06516b456a5ba86c8158eda1cb3cbc7ceb
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-15 05:45:32 -0800

    Saludar por nombre

---
+++ src/presenter copy.js
@@ -0,0 +1,29 @@
+import sumar from "./sumador";
+import multiplicar from "./multiplicador";
+
+const first = document.querySelector("#primer-numero");
+const second = document.querySelector("#segundo-numero");
+const third = document.querySelector("#tercer-numero");
+const fourth = document.querySelector("#cuarto-numero");
+const form = document.querySelector("#sumar-form");
+const form2 = document.querySelector("#multiplicar-form");
+const div1 = document.querySelector("#resultado-suma");
+const div2 = document.querySelector("#resultado-multiplicacion");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const firstNumber = Number.parseInt(first.value);
+  const secondNumber = Number.parseInt(second.value);
+
+  div1.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+});
+
+form2.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const thirdNumber = Number.parseInt(third.value);
+  const fourthNumber = Number.parseInt(fourth.value);
+
+  div2.innerHTML = "<p>" + multiplicar(thirdNumber, fourthNumber) + "</p>";
+});
--- src/presenter.js
+++ src/presenter.js
@@ -1,29 +1,11 @@
-import sumar from "./sumador";
-import multiplicar from "./multiplicador";
-
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const third = document.querySelector("#tercer-numero");
-const fourth = document.querySelector("#cuarto-numero");
-const form = document.querySelector("#sumar-form");
-const form2 = document.querySelector("#multiplicar-form");
-const div1 = document.querySelector("#resultado-suma");
-const div2 = document.querySelector("#resultado-multiplicacion");
+const nombre_input = document.querySelector("#nombre");
+const form = document.querySelector("#saludar-form");
+const div = document.querySelector("#resultado-div");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const nombre = nombre_input.value;
 
-  div1.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
-});
-
-form2.addEventListener("submit", (event) => {
-  event.preventDefault();
-
-  const thirdNumber = Number.parseInt(third.value);
-  const fourthNumber = Number.parseInt(fourth.value);
-
-  div2.innerHTML = "<p>" + multiplicar(thirdNumber, fourthNumber) + "</p>";
+  div.innerHTML = "<p> Hola" + nombre + "</p>";
 });
