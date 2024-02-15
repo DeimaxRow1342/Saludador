25089d90ef3c03de48ea4fdc363a8f81198f271f
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-15 07:20:36 -0800

    Saludar de acuerdo al genero y la edad

--- src/presenter.js
+++ src/presenter.js
@@ -1,6 +1,8 @@
 import saludar from "./saludar";
 
 const nombre_input = document.querySelector("#nombre");
+const edad_input = document.querySelector("#edad");
+const genero_input = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
@@ -8,6 +10,15 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
 
   const nombre = nombre_input.value;
+  const edad = edad_input.value;
+  const genero = genero_input.value;
 
-  div.innerHTML = "<p> Hola " + saludar(nombre) + "</p>";
+  if(genero==="M" || genero=="F"){
+    
+    div.innerHTML = "<p>" + saludar(nombre, edad, genero) + "</p>";
+  }
+  else{
+    alert.show("Ingresa un genero valido");
+  }
+
 });
--- src/saludar.js
+++ src/saludar.js
@@ -1,5 +1,15 @@
-function saludar(nombre) {
-  return nombre;
+function saludar(nombre, edad, genero) {
+  if(edad>=30){
+    if(genero==="M"){
+      return "Hola Sr " + nombre;
+    }
+    else{
+      return "Hola Sra " + nombre;
+    }
+  }
+  else{
+    return "Hola " + nombre;
+  }
 }
 
 export default saludar;
