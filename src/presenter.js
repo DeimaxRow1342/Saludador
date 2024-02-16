0ee0759754402afb4c5b793c46f73851a45c30a5
Sergio <143109097+DeimaxRow1342@users.noreply.github.com>
2024-02-16 14:31:23 -0800

    Se agrega un boton y funcionalidades  para tambien poder saludar en ingles

--- src/presenter.js
+++ src/presenter.js
@@ -6,6 +6,33 @@ const genero_input = document.querySelector("#genero");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
+const nombre_label = document.querySelector("#nombre-label");
+const edad_label = document.querySelector("#edad-label");
+const genero_label = document.querySelector("#genero-label");
+const idioma_label = document.querySelector("#idioma-label");
+const saludar_boton = document.querySelector("#saludar-boton");
+
+const espanol_radio = document.querySelector("#espanol-radio");
+const ingles_radio = document.querySelector("#ingles-radio");
+
+espanol_radio.addEventListener("change", () => {
+  nombre_label.textContent = "¿Cuál es tu nombre?:";
+  edad_label.textContent = "¿Cuál es tu edad?:";
+  genero_label.textContent = "¿Cuál es tu género? (M/F)?:";
+  idioma_label.textContent = "Idioma: ";
+  saludar_boton.value = "Saludar";
+  div.innerHTML = "";
+});
+
+ingles_radio.addEventListener("change", () => {
+  nombre_label.textContent = "What's your name?:";
+  edad_label.textContent = "What's your age?:";
+  genero_label.textContent = "What's your gender? (M/F)?:";
+  idioma_label.textContent = "Language: ";
+  saludar_boton.value = "Salute";
+  div.innerHTML = "";
+});
+
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -13,16 +40,23 @@ form.addEventListener("submit", (event) => {
   const edad = edad_input.value;
   const genero = genero_input.value;
 
-  if(genero==="M" || genero=="F"){
-    
-    div.innerHTML = "<p>" + saludar(nombre, edad, genero) + "</p>";
+  let idioma = "";
+  if (espanol_radio.checked) {
+    idioma = "espanol";
+  } else if (ingles_radio.checked) {
+    idioma = "ingles";
+  } else {
+    idioma = "espanol";
   }
-  else{
-    alert("Ingresa un genero valido");
+
+  if (genero === "M" || genero === "F") {
+    const saludo = saludar(nombre, edad, genero, idioma);
+    div.innerHTML = "<p>" + saludo + "</p>";
+  } else {
+    alert("Ingresa un género válido");
     nombre_input.value = "";
     edad_input.value = "";
     genero_input.value = "";
     div.innerHTML = "";
   }
-
 });
--- src/saludar.js
+++ src/saludar.js
@@ -1,41 +1,67 @@
-function saludar(nombre, edad, genero) {
+function saludar(nombre, edad, genero, idioma) {
   const fecha = new Date();
   const hora = fecha.getHours();
-    if(edad>=30){
-    if(genero==="M"){
-      if(hora>=0 && hora < 12){
-        return "Buenos dias Sr " + nombre;
+  let saludo = "";
+
+  if (idioma === "ingles") {
+    if (edad >= 30) {
+      if (genero === "M") {
+        if (hora >= 0 && hora < 12) {
+          saludo = "Good morning Mr " + nombre;
+        } else if (hora >= 12 && hora < 19) {
+          saludo = "Good afternoon Mr " + nombre;
+        } else if (hora >= 19 && hora < 24) {
+          saludo = "Good evening Mr " + nombre;
+        }
+      } else {
+        if (hora >= 0 && hora < 12) {
+          saludo = "Good morning Mrs " + nombre;
+        } else if (hora >= 12 && hora < 19) {
+          saludo = "Good afternoon Mrs " + nombre;
+        } else if (hora >= 19 && hora < 24) {
+          saludo = "Good evening Mrs " + nombre;
+        }
       }
-      else if(hora>=12 && hora < 19){
-        return "Buenas tardes Sr " + nombre;
-      }
-      else if(hora>=19 && hora < 24){
-        return "Buenas noches Sr " + nombre;
+    } else {
+      if (hora >= 0 && hora < 12) {
+        saludo = "Good morning " + nombre;
+      } else if (hora >= 12 && hora < 19) {
+        saludo = "Good afternoon " + nombre;
+      } else if (hora >= 19 && hora < 24) {
+        saludo = "Good evening " + nombre;
       }
     }
-    else{
-      if(hora>=0 && hora < 12){
-        return "Buenos dias Sra " + nombre;
+  } else if (idioma === "espanol") {
+    if (edad >= 30) {
+      if (genero === "M") {
+        if (hora >= 0 && hora < 12) {
+          saludo = "Buenos días Sr. " + nombre;
+        } else if (hora >= 12 && hora < 19) {
+          saludo = "Buenas tardes Sr. " + nombre;
+        } else if (hora >= 19 && hora < 24) {
+          saludo = "Buenas noches Sr. " + nombre;
+        }
+      } else {
+        if (hora >= 0 && hora < 12) {
+          saludo = "Buenos días Sra. " + nombre;
+        } else if (hora >= 12 && hora < 19) {
+          saludo = "Buenas tardes Sra. " + nombre;
+        } else if (hora >= 19 && hora < 24) {
+          saludo = "Buenas noches Sra. " + nombre;
+        }
       }
-      else if(hora>=12 && hora < 19){
-        return "Buenas tardes Sra " + nombre;
-      }
-      else if(hora>=19 && hora < 24){
-        return "Buenas noches Sra " + nombre;
+    } else {
+      if (hora >= 0 && hora < 12) {
+        saludo = "Buenos días " + nombre;
+      } else if (hora >= 12 && hora < 19) {
+        saludo = "Buenas tardes " + nombre;
+      } else if (hora >= 19 && hora < 24) {
+        saludo = "Buenas noches " + nombre;
       }
     }
   }
-  else{
-    if(hora>=0 && hora < 12){
-      return "Buenos dias " + nombre;
-    }
-    else if(hora>=12 && hora < 19){
-      return "Buenas tardes " + nombre;
-    }
-    else if(hora>=19 && hora < 24){
-      return "Buenas noches " + nombre;
-    }
-  }
+
+  return saludo;
 }
 
 export default saludar;
